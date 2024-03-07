73e319ccdd3f5233d8594016a74de409e24eb189
daradara <devdaradara@gmail.com>
2024-03-07 21:17:25 +0900

    init: 폰트 및 폴더 구조 변경

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from "styled-components"
+
+const Main = styled.div`
+  font-size: 40px;
+`
+
+function MainPage() {
+  return (
+    <Main>MainPage</Main>
+  )
+}
+
+export default MainPage
