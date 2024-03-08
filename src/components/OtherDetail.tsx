cbeabdeb7650726dd268b3a31afb6e7d19ee8f74
daradara <devdaradara@gmail.com>
2024-03-09 03:59:32 +0900

    feat: Other 섹션 추가

---
+++ src/components/OtherDetail.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import styled from "styled-components";
+
+import RankingIcon from "../assets/icons/ranking.png";
+import CodeIcon from "../assets/icons/code.png";
+import BookIcon from "../assets/icons/book.png";
+import SchoolIcon from "../assets/icons/school.png";
+
+interface OtherDetailProps {
+  type: string;
+  contents: string;
+}
+
+const DetailContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin: 12px 0px;
+`;
+
+const DetailIcon = styled.img`
+  height: 30px;
+  margin-right: 20px;
+`;
+
+const DetailText = styled.div`
+  font-family: Pretendard4;
+  font-size: 24px;
+  color: #333;
+  text-align: center;
+`;
+
+function OtherDetail({ type, contents }: OtherDetailProps) {
+  return (
+    <DetailContainer>
+      <DetailIcon
+        src={
+          type === "ranking"
+            ? RankingIcon
+            : type === "code"
+            ? CodeIcon
+            : type === "book"
+            ? BookIcon
+            : SchoolIcon
+        }
+      />
+      <DetailText>{contents}</DetailText>
+    </DetailContainer>
+  );
+}
+
+export default OtherDetail;
--- src/sections/OthersSection.tsx
+++ src/sections/OthersSection.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-scroll";
 import LinkIcon from "../assets/icons/link.png";
-import styled from 'styled-components';
+import styled from "styled-components";
+import OtherDetail from "../components/OtherDetail";
 
 const Section = styled.div`
-  height: 100vh;
   padding-top: 85px;
+  padding-bottom: 20px;
   margin-top: -100px;
   background-color: #f5f5f5;
 `;
@@ -29,15 +30,73 @@ const TitleText = styled.div`
   font-family: Pretendard9;
 `;
 
+const YearContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+  margin-left: 70px;
+`;
+
+const YearText = styled.div`
+  font-family: Pretendard6;
+  font-size: 30px;
+  color: #333;
+  margin: 12px 30px 20px 0px;
+`;
+
+const DetailContainer = styled.div`
+  width: 500px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding-left: 30px;
+  border-left: 2px solid #333;
+  margin-bottom: 30px;
+`;
+
 function OthersSection() {
   return (
     <Section id="others">
-    <Link to="others" smooth={true} duration={500}>
-      <TitleContainer>
-        <TitleLink src={LinkIcon} alt="LinkIcon" />
-        <TitleText>Others</TitleText>
-      </TitleContainer>
-    </Link>
+      <Link to="others" smooth={true} duration={500}>
+        <TitleContainer>
+          <TitleLink src={LinkIcon} alt="LinkIcon" />
+          <TitleText>Others</TitleText>
+        </TitleContainer>
+      </Link>
+      
+      <YearContainer>
+        <YearText>2021</YearText>
+        <DetailContainer>
+          <OtherDetail type="school" contents="덕성여자대학교 성적우수 입학" />
+        </DetailContainer>
+      </YearContainer>
+
+      <YearContainer>
+        <YearText>2022</YearText>
+        <DetailContainer>
+          <OtherDetail type="school" contents="덕성여자대학교 컴퓨터공학과 차석" />
+          <OtherDetail type="study" contents='개발 동아리 "Corner" 2기 활동' />
+          <OtherDetail type="study" contents="자바(JAVA) 컴스터디, CTL 스터디" />
+          <OtherDetail type="ranking" contents="DS-데이터톤 우수상" />
+        </DetailContainer>
+      </YearContainer>
+
+      <YearContainer>
+        <YearText>2023</YearText>
+        <DetailContainer>
+          <OtherDetail type="school" contents="교직이수, 교육봉사 70시간" />
+          <OtherDetail type="code" contents="ICT 이브와 (Gang Mate)" />
+          <OtherDetail type="code" contents='"Swift Coding Club" 2023 1분기 활동' />
+          <OtherDetail type="study" contents='개발 동아리 "Corner" 3기 운영진 (기획 팀장)' />
+        </DetailContainer>
+      </YearContainer>
+
+      <YearContainer>
+        <YearText>2024</YearText>
+        <DetailContainer>
+          <OtherDetail type="school" contents="ICT 한이음" />
+        </DetailContainer>
+      </YearContainer>
     </Section>
   );
 }
