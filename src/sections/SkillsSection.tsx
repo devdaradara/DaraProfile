e75e30f0afd38d9983aecc183b7416f9e036580f
daradara <devdaradara@gmail.com>
2024-03-09 18:23:45 +0900

    feat: 스킬 디테일 추가

--- src/sections/SkillsSection.tsx
+++ src/sections/SkillsSection.tsx
@@ -19,12 +19,10 @@ import SwiftIcon from "../assets/icons/skills/swift.png";
 import KotlinIcon from "../assets/icons/skills/kotlin.png";
 import AndroidStudioIcon from "../assets/icons/skills/android-studio.png";
 
-
 import JiraIcon from "../assets/icons/skills/jira.png";
 import FigmaIcon from "../assets/icons/skills/figma.png";
 import GitIcon from "../assets/icons/skills/git.png";
 import GitHubIcon from "../assets/icons/skills/github.png";
-
 
 const Section = styled.div`
   height: 100vh;
@@ -78,20 +76,61 @@ const SkillBadgeContainer = styled.div`
   padding: 20px;
 `;
 
-const SkillBadge = styled.div`
-  height: 80px;
-  background-color: #fff;
-  border-radius: 23px;
-  box-shadow: 0px 0px 10px 0px #666666;
-  margin: 0px 10px;
-`;
-
 const SkillBadgeImg = styled.img`
   width: 80px;
   height: 80px;
   object-fit: cover;
   object-position: center;
   border-radius: 23px;
+`;
+
+const SkillBadge = styled.div`
+  position: relative;
+  width: 80px;
+  height: 80px;
+  background-color: #fff;
+  border-radius: 23px;
+  box-shadow: 0px 0px 10px 0px #666666;
+  margin: 0px 10px;
+  overflow: hidden;
+  transition: box-shadow 0.3s ease;
+
+  &:hover {
+    box-shadow: 0px 0px 15px 5px #666666;
+
+    > div {
+      top: 0;
+    }
+  }
+`;
+
+const SkillOverlay = styled.div`
+  position: absolute;
+  top: 100%;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.7);
+  color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  transition: top 0.3s ease;
+  border-radius: 23px;
+`;
+
+const SkillOverlayTitle = styled.div`
+  font-size: 16px;
+  font-family: Pretendard6;
+  text-align: center; 
+  margin-bottom: 5px;
+`;
+
+const SkillOverlayText = styled.div`
+  font-size: 16px;
+  font-family: Pretendard4;
+  text-align: center; 
 `;
 
 function SkillsSection() {
@@ -103,25 +142,45 @@ function SkillsSection() {
           <TitleText>Skills</TitleText>
         </TitleContainer>
       </Link>
-      
+
       <AllSkillContainer>
         <SkillContainer>
           <SkillTitle>Front-end</SkillTitle>
           <SkillBadgeContainer>
-            <SkillBadge style={{padding: "3px"}}>
-              <SkillBadgeImg src={ReactIcon} alt="ReactIcon"/>
+            <SkillBadge style={{ padding: "3px" }}>
+              <SkillBadgeImg src={ReactIcon} alt="React" />
+              <SkillOverlay>
+                <SkillOverlayTitle>React</SkillOverlayTitle>
+                <SkillOverlayText>90%</SkillOverlayText>
+              </SkillOverlay>
             </SkillBadge>
-            <SkillBadge style={{padding: "3px"}}>
-              <SkillBadgeImg src={HtmlIcon} alt="HtmlIcon" />
+            <SkillBadge style={{ padding: "3px" }}>
+              <SkillBadgeImg src={HtmlIcon} alt="HTML" />
+              <SkillOverlay>
+                <SkillOverlayTitle>HTML</SkillOverlayTitle>
+                <SkillOverlayText>50%</SkillOverlayText>
+              </SkillOverlay>
             </SkillBadge>
-            <SkillBadge style={{padding: "3px"}}>
-              <SkillBadgeImg src={CssIcon} alt="CssIcon" />
+            <SkillBadge style={{ padding: "3px" }}>
+              <SkillBadgeImg src={CssIcon} alt="CSS" />
+              <SkillOverlay>
+                <SkillOverlayTitle>CSS</SkillOverlayTitle>
+                <SkillOverlayText>50%</SkillOverlayText>
+              </SkillOverlay>
             </SkillBadge>
-            <SkillBadge>
-              <SkillBadgeImg src={JavaScriptIcon} alt="JavaScriptIcon" />
+            <SkillBadge style={{ padding: "3px" }}>
+              <SkillBadgeImg src={JavaScriptIcon} alt="JavaScript" />
+              <SkillOverlay>
+                <SkillOverlayTitle>JavaScript</SkillOverlayTitle>
+                <SkillOverlayText>70%</SkillOverlayText>
+              </SkillOverlay>
             </SkillBadge>
-            <SkillBadge>
-              <SkillBadgeImg src={TypeScriptIcon} alt="TypeScriptIcon" />
+            <SkillBadge style={{ padding: "3px" }}>
+              <SkillBadgeImg src={TypeScriptIcon} alt="TypeScript" />
+              <SkillOverlay>
+                <SkillOverlayTitle>TypeScript</SkillOverlayTitle>
+                <SkillOverlayText>70%</SkillOverlayText>
+              </SkillOverlay>
             </SkillBadge>
           </SkillBadgeContainer>
         </SkillContainer>
@@ -129,39 +188,67 @@ function SkillsSection() {
         <SkillContainer>
           <SkillTitle>Back-end</SkillTitle>
           <SkillBadgeContainer>
-            <SkillBadge style={{padding: "3px"}}>
-              <SkillBadgeImg src={JavaIcon} alt="JavaIcon" />
+            <SkillBadge style={{ padding: "3px" }}>
+              <SkillBadgeImg src={JavaIcon} alt="Java" />
+              <SkillOverlay>
+                <SkillOverlayTitle>Java</SkillOverlayTitle>
+                <SkillOverlayText>80%</SkillOverlayText>
+              </SkillOverlay>
             </SkillBadge>
-            <SkillBadge>
-              <SkillBadgeImg src={PythonIcon} alt="PythonIcon" />
+            <SkillBadge style={{ padding: "3px" }}>
+              <SkillBadgeImg src={PythonIcon} alt="Python" />
+              <SkillOverlay>
+                <SkillOverlayTitle>Python</SkillOverlayTitle>
+                <SkillOverlayText>80%</SkillOverlayText>
+              </SkillOverlay>
             </SkillBadge>
-            <SkillBadge style={{padding: "3px"}}>
-              <SkillBadgeImg src={SpringBootIcon} alt="SpringBootIcon" />
+            <SkillBadge style={{ padding: "3px" }}>
+              <SkillBadgeImg src={SpringBootIcon} alt="Spring Boot" />
+              <SkillOverlay>
+                <SkillOverlayTitle>Spring Boot</SkillOverlayTitle>
+                <SkillOverlayText>80%</SkillOverlayText>
+              </SkillOverlay>
             </SkillBadge>
-            <SkillBadge style={{padding: "3px"}}>
-              <SkillBadgeImg src={NodeJsIcon} alt="NodeJsIcon" />
+            <SkillBadge style={{ padding: "3px" }}>
+              <SkillBadgeImg src={NodeJsIcon} alt="Node.js" />
+              <SkillOverlay>
+                <SkillOverlayTitle>Node.js</SkillOverlayTitle>
+                <SkillOverlayText>60%</SkillOverlayText>
+              </SkillOverlay>
             </SkillBadge>
-            <SkillBadge style={{padding: "3px"}}>
-              <SkillBadgeImg src={FireBaseIcon} alt="FireBaseIcon" />
+            <SkillBadge style={{ padding: "3px" }}>
+              <SkillBadgeImg src={FireBaseIcon} alt="Firebase" />
+              <SkillOverlay>
+                <SkillOverlayTitle>Firebase</SkillOverlayTitle>
+                <SkillOverlayText>60%</SkillOverlayText>
+              </SkillOverlay>
             </SkillBadge>
           </SkillBadgeContainer>
         </SkillContainer>
 
-
         <SkillContainer>
           <SkillTitle>App</SkillTitle>
           <SkillBadgeContainer>
-            <SkillBadge style={{padding: "3px"}}>
-              <SkillBadgeImg src={ReactIcon} alt="ReactIcon"/>
+            <SkillBadge style={{ padding: "3px" }}>
+              <SkillBadgeImg src={SwiftIcon} alt="Swift" />
+              <SkillOverlay>
+                <SkillOverlayTitle>Swift</SkillOverlayTitle>
+                <SkillOverlayText>70%</SkillOverlayText>
+              </SkillOverlay>
             </SkillBadge>
-            <SkillBadge>
-              <SkillBadgeImg src={SwiftIcon} alt="SwiftIcon" />
+            <SkillBadge style={{ padding: "3px" }}>
+              <SkillBadgeImg src={KotlinIcon} alt="Kotlin" />
+              <SkillOverlay>
+                <SkillOverlayTitle>Kotlin</SkillOverlayTitle>
+                <SkillOverlayText>60%</SkillOverlayText>
+              </SkillOverlay>
             </SkillBadge>
-            <SkillBadge style={{padding: "3px"}}>
-              <SkillBadgeImg src={KotlinIcon} alt="KotlinIcon" />
-            </SkillBadge>
-            <SkillBadge style={{padding: "3px"}}>
-              <SkillBadgeImg src={AndroidStudioIcon} alt="AndroidStudioIcon" />
+            <SkillBadge style={{ padding: "3px" }}>
+              <SkillBadgeImg src={AndroidStudioIcon} alt="Android Studio" />
+              <SkillOverlay>
+                <SkillOverlayTitle>Android Studio</SkillOverlayTitle>
+                <SkillOverlayText>65%</SkillOverlayText>
+              </SkillOverlay>
             </SkillBadge>
           </SkillBadgeContainer>
         </SkillContainer>
@@ -169,17 +256,33 @@ function SkillsSection() {
         <SkillContainer>
           <SkillTitle>Tools</SkillTitle>
           <SkillBadgeContainer>
-            <SkillBadge style={{padding: "3px"}}>
-              <SkillBadgeImg src={JiraIcon} alt="JiraIcon" />
+            <SkillBadge style={{ padding: "3px" }}>
+              <SkillBadgeImg src={JiraIcon} alt="Jira" />
+              <SkillOverlay>
+                <SkillOverlayTitle>Jira</SkillOverlayTitle>
+                <SkillOverlayText>80%</SkillOverlayText>
+              </SkillOverlay>
             </SkillBadge>
-            <SkillBadge style={{padding: "3px"}}>
-              <SkillBadgeImg src={FigmaIcon} alt="FigmaIcon" />
+            <SkillBadge style={{ padding: "3px" }}>
+              <SkillBadgeImg src={FigmaIcon} alt="Figma" />
+              <SkillOverlay>
+                <SkillOverlayTitle>Figma</SkillOverlayTitle>
+                <SkillOverlayText>90%</SkillOverlayText>
+              </SkillOverlay>
             </SkillBadge>
-            <SkillBadge style={{padding: "3px"}}>
-              <SkillBadgeImg src={GitIcon} alt="GitIcon" />
+            <SkillBadge style={{ padding: "3px" }}>
+              <SkillBadgeImg src={GitIcon} alt="Git" />
+              <SkillOverlay>
+                <SkillOverlayTitle>Git</SkillOverlayTitle>
+                <SkillOverlayText>90%</SkillOverlayText>
+              </SkillOverlay>
             </SkillBadge>
-            <SkillBadge style={{padding: "3px"}}>
-              <SkillBadgeImg src={GitHubIcon} alt="GitHubIcon" />
+            <SkillBadge style={{ padding: "3px" }}>
+              <SkillBadgeImg src={GitHubIcon} alt="GitHub" />
+              <SkillOverlay>
+                <SkillOverlayTitle>GitHub</SkillOverlayTitle>
+                <SkillOverlayText>90%</SkillOverlayText>
+              </SkillOverlay>
             </SkillBadge>
           </SkillBadgeContainer>
         </SkillContainer>
