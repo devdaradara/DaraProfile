54d558cff20a6e516809b2a3ef042f90edb889e8
daradara <devdaradara@gmail.com>
2024-03-10 17:08:58 +0900

    fix: 프로젝트 데이터 이미지 주소 변경

--- src/data/ProjectData.ts
+++ src/data/ProjectData.ts
@@ -4,21 +4,21 @@ export const ProjectData = [
     id: 'project1',
     title: '추억 하나 주면\n안 잡아먹묘!',
     description: '타인이 만들어주는 자신만의\n추억 갤러리 웹사이트입니다.',
-    image: 'DaraProfile/images/MoonRabbit/MoonRabbit1.png',
+    image: 'images/MoonRabbit/MoonRabbit1.png',
     techStack: 'React',
   },
   {
     id: 'project2',
     title: 'Gang Mate',
     description: '갱년기 여성의 신체적, 정서적\n건강을 위한 앱입니다.',
-    image: 'DaraProfile/images/GangMate/GangMate1.png',
+    image: 'images/GangMate/GangMate1.png',
     techStack: 'React-Native',
   },
   {
     id: 'project3',
     title: 'StarHub',
     description: '지도를 이용한 스터디 및\n프로젝트 구인 사이트입니다.',
-    image: 'DaraProfile/images/StarHub/StarHub1.png',
+    image: 'images/StarHub/StarHub1.png',
     techStack: 'React, Sprig-Boot',
   },
   {
@@ -32,7 +32,7 @@ export const ProjectData = [
     id: 'project5',
     title: 'BuddyFriends',
     description: '반려 동물 돌봄 품앗이\n웹 사이트 입니다.',
-    image: 'DaraProfile/images/BuddyFriends/BuddyFriends.png',
+    image: 'images/BuddyFriends/BuddyFriends.png',
     techStack: 'React, Spring-Boot',
   }
 ];
